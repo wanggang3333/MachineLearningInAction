d8ff3a3699f62c14f8e4a15dee288240c2053a58
wanggang <wanggang3333@gmail.com>
2015-11-19 22:51:30 +0800

    finish chapter5

---
+++ chapter5/test.py
@@ -0,0 +1,8 @@
+import logRegres
+#dataArr, labelMat = logRegres.loadDataSet()
+#weights = logRegres.gradAscent(dataArr, labelMat)
+#weights = logRegres.stocGradAscent1(dataArr, labelMat)
+#logRegres.plotBestFit(weights)
+
+logRegres.multiTest()
+#logRegres.colicTest()
